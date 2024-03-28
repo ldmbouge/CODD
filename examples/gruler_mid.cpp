7334b13840d8ed82be29cad68346e3f91ae795f5
vanhoeve <vanhoeve@andrew.cmu.edu>
2024-03-28 18:47:22 -0400

    corrected an error in lb for gruler_mid

--- examples/gruler_mid.cpp
+++ examples/gruler_mid.cpp
@@ -11,7 +11,7 @@
 #include <math.h>
 
 //typedef std::set<int> GRSet;
-typedef NatSet<6> GRSet;  // 6 double-word (384 labels)
+typedef NatSet<13> GRSet;  // 13 double-word (832 labels)
 
 
 struct SGRuler {
@@ -100,7 +100,13 @@ int main(int argc,char* argv[])
                        (int)std::floor((L+1)/2) - OPT[std::floor(n/2)-s.k]});
       else 
         ub = std::min({(int)bnds.getPrimal() -1 - OPT[n-s.k],L+1 - OPT[n-s.k]});
-      auto lb = std::max({s.e+1,(int)std::ceil(s.k * (s.k -1)/2),OPT[s.k+1]});
+      
+      auto lb = s.e+1;
+      if (s.k < n-1) 
+        lb = std::max({lb,(int)std::ceil(s.k * (s.k -1)/2),OPT[s.k+1]});
+      else  
+        lb = std::max({lb,(int)std::ceil(s.k * (s.k -1)/2),OPT[s.k]+1});
+      
       return Range::closeInc(lb,ub);
    };
    const auto stf = [n](const SGRuler& s,const int label) -> std::optional<SGRuler> {
