46385cf8f1f9afc1f86859c9ed392e685f9ebab3
Laurent Michel <ldm@engr.uconn.edu>
2025-08-15 11:29:29 +0100

    fixed Exact issue

--- src/node.hpp
+++ src/node.hpp
@@ -54,6 +54,7 @@ protected:
    void addArc(Edge::Ptr ep);
 public:
    friend class AbstractDD;
+   friend class Exact;
    friend class Relaxed;
    friend class Restricted;
    friend class RestrictedND;
